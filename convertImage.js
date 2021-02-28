2e367020e752b8c0dd86232f51f3541478822312
Neha Gupta <nehagupta150498@gmail.com>
2021-02-28 21:21:40 +0530

    Complete Task

--- convertImage.js
+++ convertImage.js
@@ -2,23 +2,25 @@
  const imageminJpegtran = require('imagemin-jpegtran');
  const imageminPngquant = require('imagemin-pngquant');
  
- function webpImage(url) {
-    (async () => {
+ async function webpImage(url) {
+    //(async (url) => {
         console.log(url);
         console.log(typeof(url));
         console.log(typeof('images/download.jpg'));
-        const files = await imagemin(['images/download.jpg'], {    
-        destination: 'build/images',
-            plugins: [
-                imageminJpegtran(),
-                imageminPngquant({
-                    quality: [0.6, 0.8]
-                })
-            ]
-        });
+        const files = await imagemin([url], {  
+            destination: 'build/images',
+                plugins: [
+                    imageminJpegtran(),
+                    imageminPngquant({
+                        quality: [0.6, 0.8]
+                    })
+                ],
+            }
+        );
      
         console.log(files);
         //=> [{data: <Buffer 89 50 4e …>, destinationPath: 'build/images/foo.jpg'}, …]
-    })();
+    //})();
+ 
 }
  module.exports = webpImage;
